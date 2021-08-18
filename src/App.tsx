8769ad9e3c523d38b9568aa4e08073cbda08990c
chaidong <myreact@163.com>
2021-08-18 14:37:08 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import style from './App.module.css';
+import React from 'react'
+function App() {
+  return (
+    <div className={style.App}>
+      <img src={logo} alt="" />
+    </div>
+  );
+}
+
+export default App;
